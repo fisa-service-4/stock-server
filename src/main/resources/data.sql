5d3414189c7f611d15d2b8c96dea66f57889a17e
hyun7931 <shlee4185@gmail.com>
2026-05-21 17:31:46 +0900

    chore(stock): application.yaml에 인코딩 설정, data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,7 +17,7 @@ WHEN NOT MATCHED THEN
   INSERT (stock_code, stock_name, market_type) VALUES (s.stock_code, s.stock_name, s.market_type);
 
 MERGE INTO stock_master t
-USING (SELECT '035720' AS stock_code, '카카오' AS stock_name, 'KOSDAQ' AS market_type FROM DUAL) s
+USING (SELECT '035720' AS stock_code, '카카오' AS stock_name, 'KOSPI' AS market_type FROM DUAL) s
 ON (t.stock_code = s.stock_code)
 WHEN NOT MATCHED THEN
   INSERT (stock_code, stock_name, market_type) VALUES (s.stock_code, s.stock_name, s.market_type);
