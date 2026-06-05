a1119be3a8b718a84ea49b41cc7027cdcf987437
hyun7931 <shlee4185@gmail.com>
2026-06-05 10:08:39 +0900

    fix(stock): import 누락 수정

--- src/main/java/com/stock/domain/account/repository/SecuritiesAccountRepository.java
+++ src/main/java/com/stock/domain/account/repository/SecuritiesAccountRepository.java
@@ -2,9 +2,11 @@ package com.stock.domain.account.repository;
 
 import com.stock.domain.account.entity.SecuritiesAccount;
 import com.stock.domain.account.entity.enums.AccountStatus;
+import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface SecuritiesAccountRepository extends JpaRepository<SecuritiesAccount, Long> {
 
