e6836f50cd782f352b8e8d99985e043e5c4b5cd7
hyun7931 <119102748+hyun7931@users.noreply.github.com>
2026-06-14 16:51:45 +0900

    feat(stock): 추가 테스트 코드 작성

--- src/test/java/com/stock/StockServerApplicationTests.java
+++ src/test/java/com/stock/StockServerApplicationTests.java
@@ -1,10 +1,24 @@
 package com.stock;
 
+import com.stock.global.config.DataInitializer;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@TestPropertySource(
+    properties = {
+      "spring.jpa.hibernate.ddl-auto=none",
+      "spring.jpa.database-platform=org.hibernate.dialect.OracleDialect",
+      "spring.sql.init.mode=never",
+      "spring.jpa.defer-datasource-initialization=false"
+    })
 class StockServerApplicationTests {
+
+  @MockitoBean DataSource dataSource;
+  @MockitoBean DataInitializer dataInitializer;
 
   @Test
   void contextLoads() {}
